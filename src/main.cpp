0a78ff51a4ee6fc39686718191133a0bbee1e2dc
Jean-Michel Frouin <jm@frouin.me>
2025-07-02 21:44:50 +0200

    Update build number to 14 and include version details in program output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <core/version.h>
 
 int main() {
-    std::cout << "Trader " << std::endl;
+    std::cout << "Trader " << VERSION_INFO_STRING << std::endl;
     return 0;
 }
